3447bf58538872e49a3ab250a6381529a9cd03a5
tokotsaga <tornike.tsagareishvili@gmail.com>
2023-03-03 20:25:02 +0400

    added first part in the JS

--- javascript.js
+++ javascript.js
@@ -1,1 +1,13 @@
-function getComputerChoice ()
+function getComputerChoice () {
+  let choice = ["Rock", "Paper", "Scissors"];
+  let computerSelection =  choice[Math.floor(Math.random() * choice.length)];
+  return computerSelection;}
+  
+  function playRound(playerSelection, computerSelection) {
+    if (playerSelection == "Rock" && computerSelection == "Paper") { 
+      return "You lose. Paper beats Rock";
+    }
+  }
+const playerSelection = "Rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
