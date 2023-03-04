b2be6772d317dcbb121445bcf488ca11a9bd07cb
tokotsaga <tornike.tsagareishvili@gmail.com>
2023-03-04 20:58:49 +0400

    added alert and prompt

--- javascript.js
+++ javascript.js
@@ -3,11 +3,43 @@ function getComputerChoice () {
   let computerSelection =  choice[Math.floor(Math.random() * choice.length)];
   return computerSelection;}
   
+// prompt for player
+  let playerSelection = (prompt("Please enter your choice here"));
+  
+
+  
   function playRound(playerSelection, computerSelection) {
     if (playerSelection == "Rock" && computerSelection == "Paper") { 
       return "You lose. Paper beats Rock";
     }
+    else if (playerSelection == "Rock" && computerSelection == "Scissors") {
+      return "You win. Rock beats Scissors"
+    }
+    else if (playerSelection == "Paper" && computerSelection == "Scissors") {
+      return "You lose. Scissors beat Paper"
+    }
+    else if (playerSelection == "Paper" && computerSelection == "Rock") {
+      return "You win. Paper beats Rock"
+    }
+    else if (playerSelection == "Scissors" && computerSelection == "Rock") {
+      return "You lose. Rock beats Scissors"
+    }
+    else if (playerSelection == "Scissors" && computerSelection == "Paper") {
+      return "You win. Scissors beat Paper"
+    }
+    else if (playerSelection == "Rock" && computerSelection  =="Rock") {
+      return "Tie"
+    }
+    else if (playerSelection == "Paper" && computerSelection  =="Paper") {
+      return "Tie"
+    }
+    else if (playerSelection == "Scissors" && computerSelection  =="Scissors") {
+      return "Tie"
+    }
   }
-const playerSelection = "Rock";
+
+
 const computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection));
+alert ("Computer chooses" + " " +computerSelection)
+alert (playRound(playerSelection,computerSelection))
